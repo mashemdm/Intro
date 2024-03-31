8a8a8f576a20079b478ba78fff95eb559150e1ce
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-31 18:08:29 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -5,7 +5,7 @@ st.title("Mi primera chamba!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('zy.jpg')
+image = Image.open('Bang chan.jpeg')
 
 st.image(image, caption='Interfaces multimodales')
 
